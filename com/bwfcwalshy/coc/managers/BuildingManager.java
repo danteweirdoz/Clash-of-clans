a8ab6187d08b6bf239ac09922180b2e92b1f99d2
bwfcwalshyPluginDev <bwfcwalshy@gmail.com>
2014-12-10 13:23:48 +0000

    Update BuildingManager.java

--- com/bwfcwalshy/coc/managers/BuildingManager.java
+++ com/bwfcwalshy/coc/managers/BuildingManager.java
@@ -2,4 +2,25 @@ package com.bwfcwalshy.coc.managers;
 
 public class BuildingManager {
 
+    Main main;
+    public BuildingManager(Main pl){
+        this.main = pl;
+  }
+
+    public void createBuilding(int id, String name, Buildings type{
+        
+    }
+    public Location getBuildingLocation(int id){
+        
+    }
+    public Buildings getBuilding(int id){
+        
+    }
+    public int getBuildingId(String name){
+        id = 0;//Get id
+        return id;
+    }
+    public String getBuildingName(int id){
+        
+    }
 }
